012b79712e76fe38fc2039b8008b5c5f4e99e193
Anu <anuradha.a@aeturnum.com>
2024-03-19 22:52:23 +0530

    init

---
+++ Messenger/Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Anuradha Andriesz on 2024-03-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
